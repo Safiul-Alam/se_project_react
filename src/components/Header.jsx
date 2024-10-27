345ee4ed951ac7f35d18972408782835ed9cd688
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-10-27 12:28:05 -0400

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,16 @@ import avatar from "../images/avatar.svg"
 
 
 function Header({ handleAddClick }) {
-
+    const currentDate = new Date().toLocaleString('default', { 
+        month: 'long', 
+        day: 'numeric' 
+    });
     return (
         <header className="header">
             <div className="header__group">
-                <img className="header__logo" src={logo} />
-                <p className="header__date-and-location">June 15, New York</p>
+                <img className="header__logo" src={logo} alt="header logo"/>
+                {/* <p className="header__date-and-location">June 15, New York</p> */}
+                <p className="header__date-and-location">{currentDate}</p>
             </div>
             <div className="header__group">
                 <button onClick={handleAddClick} type="button" className="header__clothes-add-btn">+ Add clothes</button>
