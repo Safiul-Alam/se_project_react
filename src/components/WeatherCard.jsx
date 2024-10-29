c9a1d3b3d72a03039d020e10f188acacc4bc2fce
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-10-28 22:11:08 -0400

    .

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -31,7 +31,7 @@ const foundOption = weatherOptions.find((option) => {
     
   return (
     <section className="weather-card">
-      <p className="weather-card__temp">{weatherData.temp.F} &deg;</p>
+      <p className="weather-card__temp">{weatherData.temp.F} &deg;F</p>
       <img
         src={weatherOption?.url}
         alt={weatherOption?.condition}
