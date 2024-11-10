821df266c210be230c2723739304f2a0146c080a
Safiul alam, Mohammad <alam.buff@gmail.com>
2024-11-09 20:46:03 -0500

    .

--- src/components/ItemModal.jsx
+++ src/components/ItemModal.jsx
@@ -1,15 +1,25 @@
 import "../blocks/ItemModal.css";
 
-function ItemModal({ activeModal, onClose, cardData }) {
+function ItemModal({ activeModal, onClose, cardData, handleDeleteClick }) {
   return (
     <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+     
       <div className="modal__content modal__content_type_image">
-      <button onClick={onClose} type="button" className="modal__close"></button>
+        <button onClick={onClose} type="button" className="modal__close"></button>
         <img src={cardData.imageUrl} alt={`Image of ${cardData.name}`} className="modal__image" />
+        
         <div className="modal__footer">
           <h2 className="modal__caption">{cardData.name}</h2>
           <p className="modal__weather">Weather: {cardData.weather}</p>
         </div>
+        
+        <button
+          type="button"
+          className="item-modal__delete-btn"
+          onClick={handleDeleteClick}
+        >
+          Delete Item
+        </button>
       </div>
     </div>
   );
